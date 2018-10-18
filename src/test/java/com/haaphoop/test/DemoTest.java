dbf354dc8c230444df223261409f39e76a467495
haaphoop <314003129@qq.com>
2018-10-18 09:13:48 +0800

    TestNG

---
+++ src/test/java/com/haaphoop/test/DemoTest.java
@@ -0,0 +1,12 @@
+package com.haaphoop.test;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class DemoTest {
+  @Test
+  public void f() {
+	  Assert.assertEquals("1222", "1222");
+  }
+}
